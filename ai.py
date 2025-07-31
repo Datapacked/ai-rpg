2071f8f8f1a1325c18cdca47803e178f0212140e
Datapacked <gaymario42069@gmail.com>
2025-07-31 01:15:36 -0400

    made AI function to ask AI for RPG things

--- ai.py
+++ ai.py
@@ -1,26 +1,61 @@
 import requests
 
+AI_MODEL = "deepseek-r1:1.5b"
+
 IP = "127.0.0.1"
 OLLAMA_PORT = 11434
 
 GENERATE_API = f"http://{IP}:{OLLAMA_PORT}/api/generate"
 
-R = requests.post(GENERATE_API, json={
-	"model": "deepseek-r1:1.5b",
-	"prompt": "what is 2 + 4? remember the result",
-	"stream": False,
-	"keep_alive": "5m"
-}).json()
+
+def AI(prompt, context=False):
+	if not context:
+		ret = requests.post(
+			GENERATE_API,
+			json={
+				"model": AI_MODEL,
+				"prompt": "what is 2 + 4? remember the result",
+				"stream": False,
+				"keep_alive": "5m",
+			},
+		).json()
+	else:
+		a = requests.post(
+			GENERATE_API,
+			json={
+				"model": AI_MODEL,
+				"prompt": "what is 2 + 4? remember the result",
+				"stream": False,
+				"keep_alive": "5m",
+				"context": context,
+			},
+		).json()
+	
+	return (ret["response"], ret["context"])
+
+
+R = requests.post(
+	GENERATE_API,
+	json={
+		"model": AI_MODEL,
+		"prompt": "what is 2 + 4? remember the result",
+		"stream": False,
+		"keep_alive": "5m",
+	},
+).json()
 
 print(R["response"])
 
 print("--- --- ---")
 
-R = requests.post(GENERATE_API, json={
-	"model": "deepseek-r1:1.5b",
-	"prompt": "what 3 times the result?",
-	"stream": False,
-	"context": R["context"]
-}).json()
+R = requests.post(
+	GENERATE_API,
+	json={
+		"model": AI_MODEL,
+		"prompt": "what 3 times the result?",
+		"stream": False,
+		"context": R["context"],
+	},
+).json()
 
 print(R["response"])
--- game.py
+++ game.py
@@ -3,3 +3,10 @@ import art
 a = art.text2art("AI  RPG", font='medium', space=1)
 
 print(a)
+
+import time, asyncio
+
+async def fun():
+	await asyncio.sleep(4)
+	return False
+
